cff735f5c8dea27ab14955bd4f321269c1b27b22
mfalconaro <matiasfalconaro@gmail.com>
2024-08-02 14:14:25 -0300

    Fix last updated timestamp

--- build.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const buildTimestamp = new Date().toISOString();
-const envContent = `LAST_BUILD_TIMESTAMP=${buildTimestamp}\n`;
-
-fs.writeFileSync(path.join(__dirname, '.env'), envContent);
-
-console.log(`Build timestamp set to: ${buildTimestamp}`);
